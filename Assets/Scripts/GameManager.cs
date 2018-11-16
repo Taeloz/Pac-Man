0cc7a9b5fdc3992d91b785f50f5ed5e228a2b500
Matthew Brunton <matthewagbrunton@gmail.com>
2018-11-16 15:03:57 -0500

    Created game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum States
+{
+    INTRO,
+    CHASE,
+    SCATTER,
+    FLEE,
+    DEATH,
+    GAMEOVER
+};
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private States gameState;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameManager();
+            }
+
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        gameState = States.INTRO;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
