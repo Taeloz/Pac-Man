fd014390da35bb78e4d1c25eb1461a358d4de18d
Matthew Brunton <matthewagbrunton@gmail.com>
2018-11-16 20:48:55 -0500

    Added pellet spawner.

---
+++ Assets/PelletSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletSpawner : MonoBehaviour
+{
+    public GameObject pellet;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnPellets();
+    }
+
+    private void SpawnPellets()
+    {
+        for (float i = 0; i <= 14; i += 0.5f)
+        {
+            for (float j = 0; j <= 13; j += 0.5f)
+            {
+                // Don't spawn in the center, or near the warp tunnel
+                if (i >= 5 && i <= 10 && ((j >= 3 && j <= 10) || (j <= 2) || (j >= 11)))
+                {
+                    continue;
+                }
+
+                Vector2 pos = new Vector2(j, i);
+
+                // Don't spawn if intersecting a wall
+                if (Physics2D.OverlapCircle(pos, 0.3f) != null)
+                {
+                    continue;
+                }
+
+                Instantiate(pellet, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
